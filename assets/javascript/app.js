f9dd9bfeaecc1b4c80be10558b3ce187eab10890
Richard Fitkin <rfitkin90@gmail.com>
2018-12-08 06:02:18 -0800

    pre 12-8 class

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -12,8 +12,10 @@ $(document).ready(function () {
     firebase.initializeApp(config);
     var database = firebase.database();
 
-    var trainsBeingMonitored = 0;
+    // initial train table data
+    var trainCount = 0;
     var trainDataArr = [];
+    console.log(trainDataArr);
 
     $('#submit-train').on('click', function (e) {
         e.preventDefault();
@@ -23,32 +25,34 @@ $(document).ready(function () {
         var firstTrainTime = $('#first-train-time').val().trim();
         var frequency = $('#frequency').val().trim();
 
-        // calculations
-        var firstTimeConverted = moment(firstTrainTime, 'HH:mm').subtract(1, 'y');
-        var differenceInTime = moment().diff(moment(firstTimeConverted), "m");
-        var remainder = differenceInTime % frequency;
-        var minutesAway = frequency - remainder;
+        // calculation variables
+        var firstTimeConverted;
+        var differenceInTime;
+        var remainder;
+        var minutesAway;
+        var nextTrain;
+        calculateTrainData(trainCount);
         var initialMinutesAway = minutesAway;
-        var nextTrain = moment().add(minutesAway, "m");
 
         // check for correct format
         if ((firstTrainTime.length === 5 && firstTrainTime.indexOf(":") === 2) ||
             (firstTrainTime.length === 4 && firstTrainTime.indexOf(":") === 1)) {
-            // increment train count
             // update output once every second until minutesAway changes, then update it once every minute
-            outputTrainInfo(trainsBeingMonitored);
+            setTrainInfo(trainCount);
             var secondInterval = setInterval(function () {
-                outputTrainInfo();
-                console.log(`initial: ${initialMinutesAway}`);
-                console.log(`current: ${minutesAway}`);
+                updateTrainInfo(trainCount);
+                console.log(`Initial minutes away: ${initialMinutesAway}`);
+                console.log(`Current minutes away: ${minutesAway}`);
                 if (initialMinutesAway !== minutesAway) {
                     clearInterval(secondInterval);
                     setInterval(function () {
-                        outputTrainInfo();
+                        updateTrainInfo(trainCount);
+                        console.log(trainDataArr);
                     }, 60000);
                 }
             }, 1000);
-            trainsBeingMonitored++;
+            // increment train count
+            // trainCount++;
             // clear form
             $('#train-name').val('');
             $('#destination').val('');
@@ -58,37 +62,102 @@ $(document).ready(function () {
             alert(`Incorrect time format`);
         }
 
-        function outputTrainInfo(i) {
-            // calculations
+        /* Questions:
+    
+            If you use a setInterval function w/ a parameter that is a variable, and the value of the parameter
+            changes between intervals, the setInterval starts using the new value?
+
+            How do I make it keep using the old value?
+
+            Put interval function inside train object and use "this" keyword?(this would fire every time it updates)
+
+        */
+
+
+        /* ---------------------------- Callback Functions ---------------------------- */
+        function calculateTrainData() {
             firstTimeConverted = moment(firstTrainTime, 'HH:mm').subtract(1, 'y');
             differenceInTime = moment().diff(moment(firstTimeConverted), "m");
             remainder = differenceInTime % frequency;
             minutesAway = frequency - remainder;
             nextTrain = moment().add(minutesAway, "m");
-            // output
-            console.log(`Train Name: ${trainName}`);
-            console.log(`Destination: ${destination}`);
-            console.log(`First train time: ${firstTrainTime}`);
-            console.log(`Frequency: ${Number(frequency)} mins`);
-            console.log(`Next Arrival: ${nextTrain.format('h:mm A')}`);
-            console.log(`Minutes Away: ${minutesAway}`);
-            // push train data to object in array
+        }
+
+        function createTrainDataArr(i) {
+            var secondInterval = setInterval(function () {
+                updateTrainInfo(trainCount);
+                console.log(`Initial minutes away: ${initialMinutesAway}`);
+                console.log(`Current minutes away: ${minutesAway}`);
+                if (initialMinutesAway !== minutesAway) {
+                    clearInterval(secondInterval);
+                    setInterval(function () {
+                        updateTrainInfo(trainCount);
+                        console.log(trainDataArr);
+                    }, 60000);
+                }
+            }, 1000);
             trainDataArr[i] = JSON.parse(`{
                 "trainName": "${trainName}",
                 "destination": "${destination}",
+                "frequency": "${frequency}",
                 "firstTrainTime": "${firstTrainTime}",
-                "nextArrival": "${nextTrain.format('h:mm A')}",
-                "minutesAway": "${minutesAway}"
+                "nextTrain": "${nextTrain.format('h:mm A')}",
+                "minutesAway": "${minutesAway}",
+                "updateInterval": "${secondInterval}"
             }`);
-            
-            console.log(trainDataArr);
-            // add train info to database
-            // database.ref().set({
-
-            // });
         }
 
-    });
+        function setTrainInfo(i) {
+            calculateTrainData();
+            createTrainDataArr(i);
+            console.log(trainDataArr);
+            // append train data to table
+            $('#train-table-body').append(`<tr id='train-row-${i}'></tr>`)
+            console.log(trainDataArr[i].frequency);
+            $(`#train-row-${i}`).append(`
+                <td id='trainName-${i}'>${trainDataArr[i].trainName}</td>
+                <td id='destination-${i}'>${trainDataArr[i].destination}</td>
+                <td id='frequency-${i}'>${Number(trainDataArr[i].frequency)}</td>
+                <td id='nextTrain-${i}'>${trainDataArr[i].nextTrain}</td>
+                <td id='minutesAway-${i}'>${trainDataArr[i].minutesAway}</td>
+            `);
+        }
 
+        function updateTrainDataArr(i) {
+            trainDataArr[i] = JSON.parse(`{
+                "trainName": "${trainName}",
+                "destination": "${destination}",
+                "frequency": "${frequency}",
+                "firstTrainTime": "${firstTrainTime}",
+                "nextTrain": "${nextTrain.format('h:mm A')}",
+                "minutesAway": "${minutesAway}"
+            }`);
+        }
+    
+        function updateTrainInfo(i) {
+            calculateTrainData();
+            updateTrainDataArr(i);
+            // update train info on table
+            $(`#trainName-${i}`).text(trainDataArr[i].trainName);
+            $(`#destination-${i}`).text(trainDataArr[i].destination);
+            $(`#frequency-${i}`).text(trainDataArr[i].frequency);
+            $(`#nextTrain-${i}`).text(trainDataArr[i].nextTrain);
+            $(`#minutesAway-${i}`).text(trainDataArr[i].minutesAway);
+            console.log(trainDataArr[i].minutesAway);
+            console.log(i)
+            console.log("hello is this thing working?");
+        }
+    
+        // function logOutput() {
+        //     console.log(`Train Name: ${trainName}`);
+        //     console.log(`Destination: ${destination}`);
+        //     console.log(`First train time: ${firstTrainTime}`);
+        //     console.log(`Frequency: ${Number(frequency)} mins`);
+        //     console.log(`Next Arrival: ${nextTrain.format('h:mm A')}`);
+        //     console.log(`Minutes Away: ${minutesAway}`);
+        // }
+    
+        
+    });
 
 });
