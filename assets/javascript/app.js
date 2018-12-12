dcc11d56d327caacf94a6d94dab9f105ea072cd4
Richard Fitkin <rfitkin90@gmail.com>
2018-12-12 01:39:11 -0800

    adding edit buttons

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -12,6 +12,8 @@ $(document).ready(function () {
     firebase.initializeApp(config);
     var database = firebase.database();
 
+    var currentlyEditing = false;
+
     // add train to database
     $('#submit-train').on('click', function (e) {
         e.preventDefault();
@@ -37,8 +39,34 @@ $(document).ready(function () {
         $(`#row${$(this).attr('data-id')}`).remove();
     });
 
+    // edit database
+    $(document).on('click', '.edit-btn', function (e) {
+        e.preventDefault();
+        if (currentlyEditing === false) {
+            currentlyEditing = true;
+            $(`#name${$(this).attr('data-id')}`).attr('contenteditable', true);
+            $(`#destination${$(this).attr('data-id')}`).attr('contenteditable', true);
+            $(`#frequency${$(this).attr('data-id')}`).attr('contenteditable', true);
+            $(this).attr('class', 'btn confirm-btn');
+            $(this).text('Confirm');
+        }
+    });
 
-    
+    // confirm database edit
+    $(document).on('click', '.edit-btn', function (e) {
+        e.preventDefault();
+        if (currentlyEditing === true) {
+            currentlyEditing = false;
+            $(`#name${$(this).attr('data-id')}`).attr('contenteditable', false);
+            $(`#destination${$(this).attr('data-id')}`).attr('contenteditable', false);
+            $(`#frequency${$(this).attr('data-id')}`).attr('contenteditable', false);
+            $(this).attr('class', 'btn edit-btn');
+            $(this).text('Edit');
+            
+        }
+    });
+
+
     database.ref().on('child_added', function (snap) {
         console.log(snap);
         console.log(snap.val());
@@ -48,23 +76,40 @@ $(document).ready(function () {
         $(`#row${snap.ref_.key}`).append(`<td id="name${snap.ref_.key}">${snap.val().trainName}</td>`);
         $(`#row${snap.ref_.key}`).append(`<td id="destination${snap.ref_.key}">${snap.val().destination}</td>`);
         $(`#row${snap.ref_.key}`).append(`<td id="frequency${snap.ref_.key}">${snap.val().frequency}</td>`);
-        $(`#row${snap.ref_.key}`).append(`<td id="nextArrival${snap.ref_.key}">${moment().add(calculateMinutesAway(), 'm').format('HH:mm A')}</td>`);
+        $(`#row${snap.ref_.key}`).append(`<td id="nextArrival${snap.ref_.key}">
+            ${moment().add(calculateMinutesAway(), 'm').format('LT')}</td>`);
         $(`#row${snap.ref_.key}`).append(`<td id="minutesAway${snap.ref_.key}">${calculateMinutesAway()}</td>`);
-        $(`#row${snap.ref_.key}`).append(`<td id="remove${snap.ref_.key}"><button class="btn remove-btn" data-id="${snap.ref_.key}">Remove</button></td>`)
+        $(`#row${snap.ref_.key}`).append(`<td id="remove${snap.ref_.key}">
+            <button class="btn remove-btn" data-id="${snap.ref_.key}">Remove</button></td>`);
+        $(`#row${snap.ref_.key}`).append(`<td id="edit${snap.ref_.key}">
+            <button class="btn edit-btn" data-id="${snap.ref_.key}">Edit</button></td>`);
+        $(`#edit${snap.ref_.key}`).css({
+            'min-width': `${116}px`,
+            'display': 'flex',
+            'justify-content': 'center',
+        });
         // update output once every second until minute changes, then update it once every minute
         var initialMinutesAway = calculateMinutesAway();
         var sInterval = setInterval(function () {
-            updateTable();
-            if (calculateMinutesAway() !== initialMinutesAway) {
+            if (currentlyEditing === true) {
                 clearInterval(sInterval);
-                setInterval(function () {
-                    updateTable();
-                }, 60000);
+            } else {
+                updateTable();
+                if (calculateMinutesAway() !== initialMinutesAway) {
+                    clearInterval(sInterval);
+                    var mInterval = setInterval(function () {
+                        if (currentlyEditing === true) {
+                            clearInterval(mInterval);
+                        } else {
+                            updateTable();
+                        }
+                    }, 60000);
+                }
             }
         }, 1000);
         function calculateMinutesAway() {
-            var firstTimeConverted = moment(snap.val().firstTrainTime, 'HH:mm').subtract(1, 'y');
-            var timeDifference = moment().diff(moment(firstTimeConverted), "m");
+            var firstTimeConverted = moment(snap.val().firstTrainTime, 'HH:mm');
+            var timeDifference = moment().diff(moment(firstTimeConverted), 'm');
             var remainder = timeDifference % snap.val().frequency;
             var minutesAway = snap.val().frequency - remainder;
             return minutesAway;
@@ -73,7 +118,7 @@ $(document).ready(function () {
             $(`#name${snap.ref_.key}`).text(snap.val().trainName);
             $(`#destination${snap.ref_.key}`).text(snap.val().destination);
             $(`#frequency${snap.ref_.key}`).text(snap.val().frequency);
-            $(`#nextArrival${snap.ref_.key}`).text(moment().add(calculateMinutesAway(), 'm').format('HH:mm A'));
+            $(`#nextArrival${snap.ref_.key}`).text(moment().add(calculateMinutesAway(), 'm').format('LT'));
             $(`#minutesAway${snap.ref_.key}`).text(calculateMinutesAway());
         }
     });
