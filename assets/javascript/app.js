5d2b836ffc8991927e28e285233d64415b7cfa6a
Richard Fitkin <rfitkin90@gmail.com>
2018-12-14 07:22:00 -0800

    globallized update functions

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -12,7 +12,7 @@ $(document).ready(function () {
     firebase.initializeApp(config);
     var database = firebase.database();
 
-    var currentlyEditing = false;
+    var trainArray = [];
 
     // add train to database
     $('#submit-train').on('click', function (e) {
@@ -36,73 +36,92 @@ $(document).ready(function () {
     $(document).on('click', '.remove-btn', function (e) {
         e.preventDefault();
         database.ref().child($(this).attr('data-id')).remove();
+        var i = trainArray.findIndex(i => i.firebaseID === $(this).attr('data-id'));
+        trainArray.splice(i, 1);
+        console.log(trainArray);
         $(`#row${$(this).attr('data-id')}`).remove();
     });
 
     // edit database
     $(document).on('click', '.edit-btn', function (e) {
         e.preventDefault();
-        if (currentlyEditing === false) {
-            currentlyEditing = true;
-            $(`#name${$(this).attr('data-id')}`).attr('contenteditable', true);
-            $(`#destination${$(this).attr('data-id')}`).attr('contenteditable', true);
-            $(`#frequency${$(this).attr('data-id')}`).attr('contenteditable', true);
-            $(this).attr('class', 'btn confirm-btn');
-            $(this).text('Confirm');
-        }
+        $(`#name${$(this).attr('data-id')}`).attr('contenteditable', true);
+        $(`#destination${$(this).attr('data-id')}`).attr('contenteditable', true);
+        $(`#frequency${$(this).attr('data-id')}`).attr('contenteditable', true);
+        $(this).attr('class', 'btn confirm-btn');
+        $(this).text('Confirm');
+        clearTimeout(updateTimeout);
+        clearInterval(updateInterval);
     });
 
     // confirm database edit
     $(document).on('click', '.confirm-btn', function (e) {
         e.preventDefault();
-        if (currentlyEditing === true) {
-            currentlyEditing = false;
-            $(`#name${$(this).attr('data-id')}`).attr('contenteditable', false);
-            $(`#destination${$(this).attr('data-id')}`).attr('contenteditable', false);
-            $(`#frequency${$(this).attr('data-id')}`).attr('contenteditable', false);
-            $(this).attr('class', 'btn edit-btn');
-            $(this).text('Edit');
-            database.ref().child($(this).attr('data-id')).set({
-                trainName: $(`#name${$(this).attr('data-id')}`).text(),
-                destination: $(`#destination${$(this).attr('data-id')}`).text(),
-                frequency: $(`#frequency${$(this).attr('data-id')}`).text(),
-            });
-            // $('#train-table-body').empty();
-        }
+        $(`#name${$(this).attr('data-id')}`).attr('contenteditable', false);
+        $(`#destination${$(this).attr('data-id')}`).attr('contenteditable', false);
+        $(`#frequency${$(this).attr('data-id')}`).attr('contenteditable', false);
+        $(this).attr('class', 'btn edit-btn');
+        $(this).text('Edit');
+        database.ref().child($(this).attr('data-id')).set({
+            trainName: $(`#name${$(this).attr('data-id')}`).text(),
+            destination: $(`#destination${$(this).attr('data-id')}`).text(),
+            firstTrainTime: $(`#frequency${$(this).attr('data-id')}`).attr('data-firstTrainTime'),
+            frequency: $(`#frequency${$(this).attr('data-id')}`).text(),
+        });
+        var i = trainArray.findIndex(i => i.firebaseID === $(this).attr('data-id'));
+        trainArray[i].trainName = $(`#trainName${$(this).attr('data-id')}`).text();
+        trainArray[i].destination = $(`#frequency${$(this).attr('data-id')}`).text();
+        trainArray[i].frequency = $(`#frequency${$(this).attr('data-id')}`).text();
+        var t = (60 - moment().seconds()) * 1000;
+        var updateTimeout = setTimeout(() => {
+            var updateInterval = setInterval(() => {
+                for (i = 0; i < trainArray.length; i++) {
+                    calculateMinutesAway(i);
+                    updateTable(i);
+                }
+            }, 1000);
+        }, t);
     });
 
+    // update table at each minute mark
+    var updateInterval;
+    var t = (60 - moment().seconds()) * 1000;
+    var updateTimeout = setTimeout(() => {
+        updateInterval = setInterval(() => {
+            for (i = 0; i < trainArray.length; i++) {
+                calculateMinutesAway(i);
+                updateTable(i);
+            }
+        }, 1000);
+    }, t);
 
     /*
-    
-    1. How do I clear update interval outside of on child added function?
-        (declaring setInterval variables globally doesn't work)
-        (my current method doesn't clear the minute interval unless a minute mark passes while editing)
-    
-    2. How to make a function for all of the redudant stuff between ref().on functions?
-        (currently breaks everything if I make a global scope function for them)
-
-    3. Why am I getting invalid date on firstTimeConverted in the on child changed function?
-        (works perfectly in the child added function)
-    
-    4. Why doesn't the on child added function append duplicates of the other trains when I add a new one?
-
-    5. Why doesn't appending elements in the on child changed function work?
-    
+    can you turn this for loop into forEach?(while still passing i as parameter)
     */
 
-
     database.ref().on('child_added', function (snap) {
+        trainArray.push({
+            'firebaseID': snap.ref_.key,
+            'trainName': snap.val().trainName,
+            'destination': snap.val().destination,
+            'firstTrainTime': snap.val().firstTrainTime,
+            'frequency': snap.val().frequency,
+        });
+        console.log(trainArray);
         console.log(snap);
         console.log(snap.val());
         console.log(snap.ref_.key);
+        var i = trainArray.findIndex(i => i.firebaseID === snap.ref_.key);
+        console.log(i);
         // append data to table
         $('#train-table-body').append(`<tr id="row${snap.ref_.key}">`);
         $(`#row${snap.ref_.key}`).append(`<td id="name${snap.ref_.key}">${snap.val().trainName}</td>`);
         $(`#row${snap.ref_.key}`).append(`<td id="destination${snap.ref_.key}">${snap.val().destination}</td>`);
-        $(`#row${snap.ref_.key}`).append(`<td id="frequency${snap.ref_.key}">${snap.val().frequency}</td>`);
+        $(`#row${snap.ref_.key}`).append(`<td id="frequency${snap.ref_.key}" 
+            data-firstTrainTime="${snap.val().firstTrainTime}">${snap.val().frequency}</td>`);
         $(`#row${snap.ref_.key}`).append(`<td id="nextArrival${snap.ref_.key}">
-            ${moment().add(calculateMinutesAway(), 'm').format('LT')}</td>`);
-        $(`#row${snap.ref_.key}`).append(`<td id="minutesAway${snap.ref_.key}">${calculateMinutesAway()}</td>`);
+            ${moment().add(calculateMinutesAway(i), 'm').format('LT')}</td>`);
+        $(`#row${snap.ref_.key}`).append(`<td id="minutesAway${snap.ref_.key}">${calculateMinutesAway(i)}</td>`);
         $(`#row${snap.ref_.key}`).append(`<td id="remove${snap.ref_.key}">
             <button class="btn remove-btn" data-id="${snap.ref_.key}">Remove</button></td>`);
         $(`#row${snap.ref_.key}`).append(`<td id="edit${snap.ref_.key}">
@@ -112,96 +131,26 @@ $(document).ready(function () {
             'display': 'flex',
             'justify-content': 'center',
         });
-        // update output once every second until minute changes, then update it once every minute
-        var initialMinutesAway = calculateMinutesAway();
-        var sInterval = setInterval(function () {
-            if (currentlyEditing === true) {
-                clearInterval(sInterval);
-            } else {
-                updateTable();
-                if (calculateMinutesAway() !== initialMinutesAway) {
-                    clearInterval(sInterval);
-                    var mInterval = setInterval(function () {
-                        if (currentlyEditing) {
-                            clearInterval(mInterval);
-                        } else {
-                            updateTable();
-                        }
-                    }, 60000);
-                }
-            }
-        }, 1000);
-        function calculateMinutesAway() {
-            var firstTimeConverted = moment(snap.val().firstTrainTime, 'LT');
-            var timeDifference = moment().diff(moment(firstTimeConverted), 'm');
-            var remainder = timeDifference % snap.val().frequency;
-            var minutesAway = snap.val().frequency - remainder;
-            return minutesAway;
-        }
-        function updateTable() {
-            $(`#name${snap.ref_.key}`).text(snap.val().trainName);
-            $(`#destination${snap.ref_.key}`).text(snap.val().destination);
-            $(`#frequency${snap.ref_.key}`).text(snap.val().frequency);
-            $(`#nextArrival${snap.ref_.key}`).text(moment().add(calculateMinutesAway(), 'm').format('LT'));
-            $(`#minutesAway${snap.ref_.key}`).text(calculateMinutesAway());
-        }
     });
 
     database.ref().on('child_changed', function (snap) {
-        // $('#train-table-body').append(`<tr id="row${snap.ref_.key}">`);
-        // $(`#row${snap.ref_.key}`).append(`<td id="name${snap.ref_.key}">${snap.val().trainName}</td>`);
-        // $(`#row${snap.ref_.key}`).append(`<td id="destination${snap.ref_.key}">${snap.val().destination}</td>`);
-        // $(`#row${snap.ref_.key}`).append(`<td id="frequency${snap.ref_.key}">${snap.val().frequency}</td>`);
-        // $(`#row${snap.ref_.key}`).append(`<td id="nextArrival${snap.ref_.key}">
-        //     ${moment().add(calculateMinutesAway(), 'm').format('LT')}</td>`);
-        // $(`#row${snap.ref_.key}`).append(`<td id="minutesAway${snap.ref_.key}">${calculateMinutesAway()}</td>`);
-        // $(`#row${snap.ref_.key}`).append(`<td id="remove${snap.ref_.key}">
-        //     <button class="btn remove-btn" data-id="${snap.ref_.key}">Remove</button></td>`);
-        // $(`#row${snap.ref_.key}`).append(`<td id="edit${snap.ref_.key}">
-        //     <button class="btn edit-btn" data-id="${snap.ref_.key}">Edit</button></td>`);
-        // $(`#edit${snap.ref_.key}`).css({
-        //     'min-width': `${116}px`,
-        //     'display': 'flex',
-        //     'justify-content': 'center',
-        // });
-        // update output once every second until minute changes, then update it once every minute
-        console.log(snap.ref_.key);
-        var initialMinutesAway = calculateMinutesAway();
-        var sInterval = setInterval(function () {
-            if (currentlyEditing === true) {
-                clearInterval(sInterval);
-            } else {
-                updateTable();
-                if (calculateMinutesAway() !== initialMinutesAway) {
-                    clearInterval(sInterval);
-                    var mInterval = setInterval(function () {
-                        if (currentlyEditing === true) {
-                            clearInterval(mInterval);
-                        } else {
-                            updateTable();
-                        }
-                    }, 60000);
-                }
-            }
-        }, 1000);
-        function calculateMinutesAway() {
-            console.log(snap.val().firstTrainTime);
-            var firstTimeConverted = moment(snap.val().firstTrainTime, 'LT');
-            console.log(firstTimeConverted);
-            var timeDifference = moment().diff(moment(firstTimeConverted), 'm');
-            console.log(timeDifference);
-            var remainder = timeDifference % snap.val().frequency;
-            var minutesAway = snap.val().frequency - remainder;
-            console.log(minutesAway);
-            return minutesAway;
-        }
-        function updateTable() {
-            $(`#name${snap.ref_.key}`).text(snap.val().trainName);
-            $(`#destination${snap.ref_.key}`).text(snap.val().destination);
-            $(`#frequency${snap.ref_.key}`).text(snap.val().frequency);
-            $(`#nextArrival${snap.ref_.key}`).text(moment().add(calculateMinutesAway(), 'm').format('LT'));
-            $(`#minutesAway${snap.ref_.key}`).text(calculateMinutesAway());
-        }
+        calculateMinutesAway(i);
+        updateTable(i);
     });
+
+    function calculateMinutesAway(i) {
+        var firstTimeConverted = moment(trainArray[i].firstTrainTime, 'LT');
+        var timeDifference = moment().diff(moment(firstTimeConverted), 'm');
+        var remainder = timeDifference % trainArray[i].frequency;
+        var minutesAway = trainArray[i].frequency - remainder;
+        return minutesAway;
+    }
+    function updateTable(i) {
+        $(`#name${trainArray[i].firebaseID}`).text(trainArray[i].trainName);
+        $(`#destination${trainArray[i].firebaseID}`).text(trainArray[i].destination);
+        $(`#frequency${trainArray[i].firebaseID}`).text(trainArray[i].frequency);
+        $(`#nextArrival${trainArray[i].firebaseID}`).text(moment().add(calculateMinutesAway(i), 'm').format('LT'));
+        $(`#minutesAway${trainArray[i].firebaseID}`).text(calculateMinutesAway(i));
+    }
 
 });
