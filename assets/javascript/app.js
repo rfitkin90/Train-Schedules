c51f2d990f527e1822f00d9f1e1e66fc904a751b
Richard Fitkin <rfitkin90@gmail.com>
2018-12-23 16:43:44 -0800

    added css and google auth

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -12,25 +12,44 @@ $(document).ready(function () {
     firebase.initializeApp(config);
     var database = firebase.database();
 
-    var trainArray = [];
     var provider = new firebase.auth.GoogleAuthProvider();
 
-    firebase.auth().signInWithPopup(provider).then(function (result) {
-        // This gives you a Google Access Token. You can use it to access the Google API.
-        var token = result.credential.accessToken;
-        // The signed-in user info.
-        var user = result.user;
-        // ...
-    }).catch(function (error) {
-        // Handle Errors here.
-        var errorCode = error.code;
-        var errorMessage = error.message;
-        // The email of the user's account used.
-        var email = error.email;
-        // The firebase.auth.AuthCredential type that was used.
-        var credential = error.credential;
-        // ...
-    });
+    var user = firebase.auth().currentUser;
+    console.log(user);
+    if (user || firebase.auth().getRedirectResult()) {
+       // User is signed in.
+       firebase.auth().getRedirectResult().then(function (result) {
+          console.log(result)
+
+          if (result.credential) {
+             // This gives you a Google Access Token. You can use it to access the Google API.
+             var token = result.credential.accessToken;
+          }
+          else {
+             var provider = new firebase.auth.GoogleAuthProvider();
+             console.log(1)
+             firebase.auth().signInWithRedirect(provider);
+          }
+          // The signed-in user info.
+          var user = result.user;
+       }).catch(function (error) {
+          // Handle Errors here.
+          var errorCode = error.code;
+          var errorMessage = error.message;
+          // The email of the user's account used.
+          var email = error.email;
+          // The firebase.auth.AuthCredential type that was used.
+          var credential = error.credential;
+          // ...
+       });
+
+    } else {
+       // No user is signed in.
+       console.log("No user signed in.")
+    }
+
+
+    var trainArray = [];
 
     // add train to database
     $('#submit-train').on('click', function (e) {
